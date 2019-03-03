5089693b0e781d234a7c335117be56da5ea51d14
TakimiShinoda <godgodx4@gmail.com>
2019-03-03 12:51:28 +0900

    fix by async & await

--- app.js
+++ app.js
@@ -14,24 +14,33 @@ log4js.configure({
 
 let Logger = log4js.getLogger();
 
-function mainProc(){
+async function tryFetch(url, times){
+  for(var i = 0; i < times; i++){
+    try{
+      await asyncFetch(url)
+      Logger.debug(`Success to access: ${url}`)
+      break;
+    }catch(err){
+      console.log(`Failed by Error: ${err}`);
+    }
+  }
+}
+
+async function asyncFetch(url){
   return new Promise((res, rej) => {
-    console.log(lp)
-    fetch(`${lp.arm_ip}/power?state=true`)
-      .then(() => { return fetch(`${lp.arm_ip}/hand?percentage=30`) })
-      .then(() => { return fetch(`${lp.arm_ip}/wrist?percentage=0`) })
-      .then(() => { return fetch(`${lp.arm_ip}/arm?percentage=0`) })
-      .then(() => { return fetch(`${lp.arm_ip}/arm?percentage=100`) })
-      .then(() => { return fetch(`${lp.arm_ip}/power?state=false`) })
-      .then(() => {
-         Logger.debug('Success!');
-	 res();
-      })
-      .catch((err) => {
-        Logger.debug(`Failed by Error: ${err}`);
-        rej(err);
-      });
-  })
+    fetch(url)
+      .then(() => { res() })
+      .catch(() => { rej() });
+  });
+}
+
+async function mainProc(){
+  await tryFetch(`${lp.arm_ip}/power?state=true`, 5);
+  await tryFetch(`${lp.arm_ip}/hand?percentage=30`, 5);
+  await tryFetch(`${lp.arm_ip}/wrist?percentage=0`, 5);
+  await tryFetch(`${lp.arm_ip}/arm?percentage=0`, 5);
+  await tryFetch(`${lp.arm_ip}/arm?percentage=100`, 5);
+  await tryFetch(`${lp.arm_ip}/power?state=false`, 5);
 }
 
 var switchOn = ns.scheduleJob('30 7 * * 0,1,2,4,5,6', function () {
@@ -49,7 +58,4 @@ var switchOff2 = ns.scheduleJob('0 21 * * 2,4', function () {
    .catch((err) => { console.log(err) });
 });
 
-
- mainProc()
-   .then(() => { console.log('fin') })
-   .catch((err) => { console.log(err) });
+// mainProc();
